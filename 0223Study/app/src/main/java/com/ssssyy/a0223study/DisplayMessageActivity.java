83f90f4f78953f3d50f443efc22c18eaaa2758d6
ssssyy <ssssyy2018@163.com>
2021-02-25 13:37:36 +0800

    新建fkm项目

--- 0223Study/app/src/main/java/com/ssssyy/a0223study/DisplayMessageActivity.java
+++ 0223Study/app/src/main/java/com/ssssyy/a0223study/DisplayMessageActivity.java
@@ -1,8 +1,10 @@
 package com.ssssyy.a0223study;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class DisplayMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
+
     }
 }
